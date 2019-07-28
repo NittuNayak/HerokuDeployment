9073be9b538a6d11ca007729de7e2d5f9c9cec6b
MukeshNayak <mukeshknayak@gmail.com>
2019-07-29 00:05:53 +0530

    Added mongoose

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+
 
 let PORT = process.env.PORT;
 if (PORT == null || PORT == "") {
     PORT = 8000;
 }
 
+
+
 app.get('/', (req, res) => {
     res.send("My First Heroku Deployment");
+    mongoose.connect('mongodb://mukesh:manan%401990@ds255917.mlab.com:55917/practicse', { useNewUrlParser: true }).then(()=>{
+        console.log("Connected Successfully");
+    });
 });
 
 app.listen(PORT, () => {
